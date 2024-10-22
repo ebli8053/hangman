5fac96a2e831c320f11b54dea1031e5d4c903e5d
ebli8053 <ebba.liwerstrohle.8053@student.uu.se>
2024-10-22 14:53:33 +0200

    Add files via upload

--- game_options.py
+++ game_options.py
@@ -1,3 +1,8 @@
+def letter_amount():
+    print("How many letters are in your word?\n")
+    amount_letters = input("Amount of letters: ")
+    return amount_letters
+
 def game_options(title, prompt, options):
     while True:
         print(title)
@@ -11,24 +16,20 @@ def game_options(title, prompt, options):
             return s
         else:
             print(f"invalid option: {s}. Please try again!")
-        
-
-def letter_amount():
-    print("How many letters are in your word?\n")
-    amount_letters = input("Amount of letters: ")
-    
-#splashscreen
-#menyn, spela multiplayer single player
+            
 
 options1 = {"s":"Single player", "m":"Multiplayer"}
 opt1 = game_options("Select an action", "Action: ", options1)
-print(f"You selected action {options1[opt1]}")
-print()
+#print(f"You selected {options1[opt1]}")
+
+if opt1 == "m":
+    letters = letter_amount()
+    print(f"You selected {letters} letters.\n")
+else:
+    print("Single player selected.\n")
+    
 
 options2 = {"a":"one", "b": "three", "c": "five"}
 opt2 = game_options("Select number of rounds", "Rounds: ", options2)
 print(f"You selected {options2[opt2]} rounds")
 print()
-
-letter_amount()
-
