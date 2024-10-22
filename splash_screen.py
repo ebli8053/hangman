68d2b76b031f13a6972ca724815ea223b6679863
ebli8053 <ebba.liwerstrohle.8053@student.uu.se>
2024-10-22 14:34:15 +0200

    Add files via upload

---
+++ splash_screen.py
@@ -0,0 +1,32 @@
+def splash_screen():
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("          Hangman the game")
+    print("                ___                   ")
+    print("               |x x|")
+    print("               |___|")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    
+def splash_screen2():
+    print("      __           ___             __ ")
+    print("|  | |  |   |   | |       |   |   |  |   |   |")
+    print("|--| |--|  | | |  |---|  | | | |  |--|  | | |")
+    print("|  | |  | |   |   |___| |   |   | |  | |   |")
+    
+def splash_screen3():
+    print("----------------")
+    print("|   /          |")
+    print("|  /           |")
+    print("| /           ---")
+    print("|/           |x x|")
+    print("|  Hangman    ---")
+    print("|  the game    |")
+    print("|             /|\ ")
+    print("|            / | \ ")
+    print("|           /  |  \ ")
+    print("|             / \ ")
+    print("|            /   \ ")
+    print("|           /     \ ")
+
+splash_screen()
+splash_screen2()
+splash_screen3()
