1e38c4b2b6546b9ae89f7827fa78ca712b27d4a8
ebli8053 <ebba.liwerstrohle.8053@student.uu.se>
2024-10-22 15:11:33 +0200

    Add files via upload

---
+++ winner:loser.py
@@ -0,0 +1,28 @@
+def winner():
+    print("                             ___  __")
+    print("|   |   | |   |   |   |   | |    |  |")
+    print(" | | | |  |  | | |   | | |  |--  |-|")
+    print("  |   |   | |   |   |   |   |___ |  |")
+    print("              _____ ")
+    print("             | O O |")
+    print("             |  _  |")
+    print("             |_____|")
+    
+def winner2():
+    print("You won")
+    print(" _____ ")
+    print("| O O |")
+    print("|  _  |")
+    print("|_____|")
+    
+    
+def loser():
+    print("Game over")
+    print(" _____")
+    print("| x x |")
+    print("|  -  |")
+    print("|_____|")
+    
+winner()
+winner2()
+loser()
